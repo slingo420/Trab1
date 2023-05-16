bdb4c7bb9cfd9647f684b484946a11983b8bc3fb
slingo420 <67109968+slingo420@users.noreply.github.com>
2023-05-15 22:03:40 -0300

    Add files via upload

--- trab1.cpp
+++ trab1.cpp
@@ -3,6 +3,7 @@
 #include <stack>
 #include <string>
 
+//-----------Valores de Tags Necessarios-----------//
 std::string nome_cenario;
 
 struct dimensoes {
@@ -17,23 +18,25 @@ struct robo {
 
 dimensoes dimensaomatriz;
 robo robo_;
+//-----------Variaveis para valores de Tags necessarios-----------//
 
+
+//-----------Funcao para atribuir valores para as variaveis-----------//
 void getVars(const std::string& xmlfilename) {
-    std::ifstream file(xmlfilename);
+    std::ifstream file(xmlfilename);  //cria o arquivo
 
-    std::string line;
-    while (std::getline(file, line)) {
-        size_t startPos = line.find("<");
+    std::string line; 
+    while (std::getline(file, line)) {  //loop para percorrer cada linha do arquivo 
+        size_t startPos = line.find("<");  //busca por tags 
         while (startPos != std::string::npos) {
             size_t endPos = line.find(">", startPos);
             if (endPos != std::string::npos) {
                 std::string tag = line.substr(startPos + 1, endPos - startPos - 1);
-                if (tag[0] != '/') {  // É uma tag de abertura
+                if (tag[0] != '/') {  //É uma tag de abertura
                     size_t closingTagPos = line.find("</" + tag + ">", endPos);
                     if (closingTagPos != std::string::npos) {
                         size_t valueStartPos = endPos + 1;
-                        std::string value = line.substr(valueStartPos, closingTagPos - valueStartPos);
-                        std::cout << "Tag: " << tag << ", Valor: " << value << std::endl;
+                        std::string value = line.substr(valueStartPos, closingTagPos - valueStartPos);  //adquire o valor da tag 
                         if (tag == "nome") {
                             nome_cenario = value;
                         }
@@ -49,8 +52,14 @@ void getVars(const std::string& xmlfilename) {
                         if (tag == "y") {
                             robo_.y = std::stoi(value);
                         }
-                        // encontrar uma forma de armazenar a matriz
-                        // chamar uma funcao que limpa a area da matriz
+                        if (tag == "matriz") {
+
+                        }
+                        //armazenar a matriz em uma variavel
+                        //criar uma funcao que recebe como argumento nome_cenario, dimensaomatriz.altura, dimensaomatriz.largura,
+                        //robo_.x, robo_.y e a matriz e calcula o tamanho da area que deve ser limpa pelo robo
+                        //depois, a funcao deve imprimir nome_cenario e o tamanho da area que deve ser limpa pelo robo
+                        //chamar a funcao que calcula a area da matriz e passar todos os argumentos
                     }
                 }
             }
@@ -58,10 +67,10 @@ void getVars(const std::string& xmlfilename) {
         }
     }
 }
+//-----------Funcao para atribuir valores para as variaveis-----------//
 
 int main() {
-    // Primeiro problema - begin
-    
+    //-----------Problema1: Empilhar Tags-----------//
     char xmlfilename[100];
 
     std::cin >> xmlfilename;  // entrada
@@ -77,13 +86,13 @@ int main() {
         while (startPos != std::string::npos) {
             size_t endPos = line.find(">", startPos);
             if (endPos != std::string::npos) {
-                std::string tag = line.substr(startPos + 1, endPos - startPos - 1);
-                if (tag[0] != '/') {  // É uma tag de abertura
-                    tagStack.push(tag);
-                } else {  // É uma tag de fechamento
+                std::string tag = line.substr(startPos + 1, endPos - startPos - 1);  //armazena o nome da tag na string tag
+                if (tag[0] != '/') {  //É uma tag de abertura
+                    tagStack.push(tag);  //empilha a tag
+                } else {  //É uma tag de fechamento
                     std::string closingTag = tag.substr(1);
                     if (!tagStack.empty() && tagStack.top() == closingTag) {
-                        tagStack.pop();
+                        tagStack.pop();  //desempilha a tag
                     } else {
                         std::cout << "Erro" << std::endl;
                         return 1;
@@ -94,11 +103,10 @@ int main() {
         }
     }
 
-    // Verifica se a pilha está vazia, indicando que todas as tags foram corretamente fechadas
+    // Verifica se a pilha nao esta vazia, terminando em um erro caso nao esteja
     if (!tagStack.empty()) {
         std::cout << "Erro" << std::endl;
     }
     return 0;
-
-    // Primeiro problema - end
+    //-----------Problema1: Empilhar Tags-----------//
 }
